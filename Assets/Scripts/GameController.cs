e13e808f22ca16b229e32f39258128d46de2183e
mccarm416 <mccarm416@gmail.com>
2017-10-19 20:19:58 -0400

    UI added, more sound effects added, persistent high score

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	[SerializeField]
+	Text lblLife;
+	[SerializeField]
+	Text lblScore;
+	[SerializeField]
+	Text lblHighScore;
+	[SerializeField]
+	Text lblGameOver;
+	[SerializeField]
+	Button btnRestart;
+
+
+	private int _highScore;
+	private int _score = 0;
+	private int _life = 3;
+
+	//Getters and setters
+	public int Score { 
+		get { return _score; }
+		set { _score = value; 
+			lblScore.text = "Score: " + +_score;
+		}
+	}
+
+	public int Life {
+		get { return _life; }
+		set { _life = value; 
+			//Update UI
+			if (_life <= 0) {
+				gameOver ();
+			}
+			else {
+				lblLife.text = "Life: " + _life;
+			}
+		}
+	}
+
+	public int HighScore {
+		get { return _highScore; }
+		set { _highScore = value; }
+	}
+
+	private void initialize() {
+		//Initialize the UI, scores, and life
+		_highScore = PlayerPrefs.GetInt("highScore");
+		Debug.Log("High Score is: " + _highScore);
+		_score = 0;
+		_life = 3;
+		lblGameOver.gameObject.SetActive (false);
+		lblHighScore.gameObject.SetActive (false);
+		btnRestart.gameObject.SetActive (false);
+
+		lblLife.gameObject.SetActive (true);
+		lblScore.gameObject.SetActive (true);
+	}
+
+	private void gameOver() {
+		//Show game over screen, update highscore if needed
+		if (_highScore < _score) {
+			lblHighScore.text = "High Score: " + _score;
+			_highScore = _score;
+			PlayerPrefs.SetInt ("highScore", _highScore);
+			PlayerPrefs.Save ();
+			Debug.Log ("High Score of " + _highScore + " has been saved!");
+		} 
+		else {
+			lblHighScore.text = "High Score: " + _highScore;
+		}
+		lblGameOver.gameObject.SetActive (true);
+		lblHighScore.gameObject.SetActive (true);
+		btnRestart.gameObject.SetActive (true);
+
+		lblLife.gameObject.SetActive (false);
+		lblScore.gameObject.SetActive (false);
+	}
+
+	public void btnResetClick() {
+		//Resets the game
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+	// Use this for initialization
+	void Start () {
+		initialize ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupController : MonoBehaviour {
+	[SerializeField]
+	float minYSpeed = -1f;
+	[SerializeField]
+	float maxYSpeed = 1f;
+	[SerializeField]
+	float minXSpeed = 3f;
+	[SerializeField]
+	float maxXSpeed = 4f;
 
-	// Use this for initialization
+	private Transform _transform;
+	private Vector2 _currentPos;
+	private Vector2 _currentSpeed;
+
 	void Start () {
-		
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPos = _transform.position;
+		Reset ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		_currentPos = _transform.position;
+		_currentPos -= _currentSpeed;
+		_transform.position = _currentPos;
+
+		//Checks to see if the pickup is offscreen and replaces it
+		if (_currentPos.x <= -500) {
+			Reset ();
+		}
+		if (_currentPos.y <= -352) {
+			Reset ();
+		}
+		if (_currentPos.y >= 352) {
+			Reset ();
+		}
+	}
+
+	public void Reset(){
+		float xSpeed = Random.Range (minXSpeed, maxXSpeed);
+		float ySpeed = Random.Range (minYSpeed, maxYSpeed);
+
+		_currentSpeed = new Vector2 (xSpeed, ySpeed);
+
+		//Place enemy in random location off of the screen
+		float x = Random.Range (445, 800);
+		float y = Random.Range (-150, 150);
+		_transform.position = 
+			new Vector2 (x, y);
 	}
 }
